cdbde1f8b057827f5fe03fa5ad48a234f0afd878
ahmadmardeni1 <ahmadmardeni0@gmail.com>
2023-01-05 15:10:55 +0400

    fix for Vercel

--- configureWarpClient.js
+++ configureWarpClient.js
@@ -1,12 +1,12 @@
 import { WarpFactory } from 'warp-contracts'
 import { transactionId } from './transactionid'
-import wallet from './testwallet'
+
 
 /*
 *  environment can be 'local' | 'testnet' | 'mainnet' | 'custom';
 */
 
-const environment = process.env.NEXT_PUBLIC_WARPENV || 'testnet'
+const environment = 'testnet'
 let warp
 let contract
 
@@ -14,9 +14,7 @@ async function getContract() {
   if (environment == 'testnet') {
     warp = WarpFactory.forTestnet()
     contract = warp.contract(transactionId).connect(wallet)
-  } else if (environment === 'mainnet') {
-    warp = WarpFactory.forMainnet()
-    contract = warp.contract(transactionId).connect()
+
   } else {
     throw new Error('Environment configured improperly...')
   }
